716231c104d9ba5f7013d314ff6c349929f6be40
Dilmurd <0nishonov.dilmurod@gmail.com>
2024-08-18 03:45:06 +0500

    Sass2

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const show = document.querySelector (".navbar__locations")
+const close = document.querySelector (".btn")
+function showModel(){
+    navbar__location.style.display = "block"
+}
+function closeModel(){
+    btn.style.display = "none"
+}
